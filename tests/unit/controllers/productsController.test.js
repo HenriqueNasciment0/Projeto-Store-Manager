058b3c40e1fbcefef9488b1bfdee3daf8b977e27
HenriqueNasciment0 <you@example.com>
2022-07-06 16:04:05 -0300

    adiciona test de create e delete no controller de produtos

--- tests/unit/controllers/productsController.test.js
+++ tests/unit/controllers/productsController.test.js
@@ -5,7 +5,7 @@ const sinon = require('sinon');
 const ProductsController = require('../../../controllers/productsController');
 const ProductsService = require('../../../services/productsService');
 
-describe('Verifica o código de status e se a mensagem foi enviada caso um produto não exista', () => {
+describe('Lê o endpoit Products na camada Controller', () => {
 
   describe('Ao chamar o controller de findById', () => {
 
@@ -90,12 +90,17 @@ describe('Verifica o código de status e se a mensagem foi enviada caso um produ
       const response = {}
 
       before(() => {
-        request.body = 'Homem Areia';
+        request.body = {
+          "name": "Homem areia"
+        };
 
         response.status = sinon.stub().returns(response);
         response.send = sinon.stub().returns();
 
-        sinon.stub(ProductsService, 'createProduct').resolves(true);
+        sinon.stub(ProductsService, 'createProduct').resolves({
+          "id": 4,
+          "name": "Homem areia"
+        });
 
       })
 
@@ -103,12 +108,41 @@ describe('Verifica o código de status e se a mensagem foi enviada caso um produ
       ProductsService.createProduct.restore();
       })
 
-      // it('é chamado com o código 201', async () => {
-      //   await ProductsController.createProduct(request, response);
+      it('é chamado com o código 201', async () => {
+        await ProductsController.createProduct(request, response);
 
-      //   expect(response.status.calledWith(201)).to.be.equal(true);
-      // })
+        expect(response.statusCode.calledWith(201)).to.be.equal(true);
+      })
 
   })
+
+  describe('quando deleta um produto do db', async () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      request.params = {
+        id: 1,
+      };
+
+      response.status = sinon.stub()
+        .returns(response);
+      response.json = sinon.stub()
+        .returns();
+
+      sinon.stub(ProductsService, 'findById')
+        .resolves('');
+    });
+
+    after(() => {
+      ProductsService.findById.restore();
+    });
+
+    it('é chamado o método "json" passando vazio', async () => {
+      await ProductsController.findById(request, response);
+
+      expect(response.json.calledWith(sinon.match.any)).to.be.equal(true);
+    });
+  });
 
 })
